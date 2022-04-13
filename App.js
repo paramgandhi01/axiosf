5124a99b955a1825eb6d36a6431c8858f8d621ea
paramgandhi01 <92042054+paramgandhi01@users.noreply.github.com>
2022-04-13 12:49:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Get from "./comp/Get"
+
+function App() {
+  return (
+    <div className="App">
+      <Get/>
+    </div>
+  );
+}
+
+export default App;
